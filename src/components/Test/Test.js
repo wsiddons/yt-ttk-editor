a48f36edf8327f5fa5968932637cc637d4aa00c6
wsiddons <wsiddons11@gmail.com>
2022-12-07 16:30:35 -0500

    working build

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,4 +1,5 @@
-import React, { useState, useMemo, useRef, useEffect } from 'react';
+import React, { useState, useMemo, useRef, useEffect } from 'react'
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
 
 function Test() {
     const inputVideo = useRef(null)
@@ -109,14 +110,72 @@ function Test() {
         setIsMouseDown(false)
     }
 
+    const [clipStart, setClipStart] = useState(0)
+    const [clipEnd, setClipEnd] = useState()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const ffmpeg = createFFmpeg({
+            log: true,
+        })
+
+        const multi = inputVideo.current.videoWidth / inputVideo.current.offsetWidth
+
+        await ffmpeg.load();
+
+        const file1 = e.target[0].files[0]
+        const name1 = file1.name
+
+        const w = cropperCam.current.offsetWidth * multi
+        const h = cropperCam.current.offsetHeight * multi
+        const x = cropperCam.current.offsetLeft * multi
+        const y = cropperCam.current.offsetTop * multi
+
+        const w2 = cropperVideo.current.offsetWidth * multi
+        const h2 = cropperVideo.current.offsetHeight * multi
+        const x2 = cropperVideo.current.offsetLeft * multi
+        const y2 = cropperVideo.current.offsetTop * multi
+
+        const cropCam = `crop=${w}:${h}:${x}:${y},scale=720:426`
+        const cropVideo = `crop=${w2}:${h2}:${x2}:${y2},scale=720:854`
+        console.log(cropCam)
+        ffmpeg.FS('writeFile', name1, await fetchFile(file1))
+
+        // set clip time
+        await ffmpeg.run( '-ss', clipStart, '-i', name1, '-to', clipEnd, '-c:v', 'libx264', '-c:a', 'aac', 'trim.mp4')
+        //crop clip 1 -c:v libx264 -c:a aac
+        await ffmpeg.run('-i', 'trim.mp4', '-filter:v', cropCam, '-c:v', 'libx264', '-preset', 'ultrafast', 'crop1.mp4')
+        //crop clip 2
+        await ffmpeg.run('-i', 'trim.mp4', '-filter:v', cropVideo, '-c:v', 'libx264', '-preset', 'ultrafast', 'crop2.mp4')
+
+        //stack the cropped clips
+        await ffmpeg.run('-i', 'crop1.mp4', '-i', 'crop2.mp4', '-filter_complex', 'vstack=inputs=2', 'output.mp4')
+
+        //ffmpeg -i input.mp4 -filter:v "crop=100:100:10:20,scale=200:200" output.mp4
+
+        //read the output
+        const data = ffmpeg.FS('readFile', `output.mp4`);
+        const a = document.createElement('a')
+        a.download = `dl`
+        a.href = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+        a.innerHTML = `download`
+        document.body.appendChild(a)
+    }
+
     const handleUpload = (e) => {
         const file = e.target.files[0]
         inputVideo.current.src = URL.createObjectURL(file)
+        console.dir(inputVideo.current)
+        // console.log(`crop=${cropperCam.current.offsetWidth}:${cropperCam.current.offsetHeight}:${cropperCam.current.offsetLeft}:${cropperCam.current.offsetTop}`)
+        // console.log(`crop=${cropperVideo.current.offsetWidth}:${cropperVideo.current.offsetHeight}:${cropperVideo.current.offsetLeft}:${cropperVideo.current.offsetTop}`)
     }
+
+    console.dir(inputVideo.current)
+    // console.dir(cropperVideo.current)
 
     return (
         <div className='page-container'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input onChange={handleUpload} type='file' />
                 <button>Make Clip</button>
             </form>
@@ -189,6 +248,10 @@ function Test() {
                         ></div>
                 </div>
             </div>
+            <p>clip start (sec)</p>
+            <input onChange={(e) => setClipStart(e.target.value)} type='number' />
+            <p>clip end (sec)</p>
+            <input onChange={(e) => setClipEnd(e.target.value)} type='number' />
         </div>
     ) 
 }
